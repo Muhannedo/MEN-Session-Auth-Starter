8b9210589f23d4add336bc9e4a1e7d4f55722f0c
Muhannad <muhanned9980@gmail.com>
2024-10-20 11:09:05 +0300

    adding the controllers authintication

---
+++ controllers/auth.js
@@ -0,0 +1,6 @@
+//
+const express = require("express");
+//create routes using the  express content 
+const router = express.Router();
+// to berdge the two pages make link with the server.js 
+module.exports = router;
