eb10a633d63f93be1d76f3935128331c3f1ec60c
Muhannad <muhanned9980@gmail.com>
2024-10-20 16:29:20 +0300

    setup DB

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
-const session = require('express-session');
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
 require("dotenv").config();
 require("./config/dataBase.js");
 // CONTROLLER
@@ -20,33 +22,36 @@ app.use(methodOverride("_method"));
 app.use(morgan("dev"));
 
 app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: true,
-    })
-  );
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
 // ROUTES - public
 
 // server.js
 
 // GET /test the route
 app.get("/", async (req, res) => {
-    res.render("index.ejs", {
-        user: req.session.user,
-      });
+  res.render("index.ejs", {
+    user: req.session.user,
+  });
 });
 
 app.use("/auth", authController);
 // protected routes
-app.get('/protected' , async (req, res)=>{
-    if(req.session.user){
-        res.send(`Welcome to the party ${req.session.user.username}.`);
-    }else{
-        res.sendStatus(404);
-        // res.send("Sorry, no guests allowed.");
-    }
-})
+app.get("/protected", async (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome to the party ${req.session.user.username}.`);
+  } else {
+    res.sendStatus(404);
+    // res.send("Sorry, no guests allowed.");
+  }
+});
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
