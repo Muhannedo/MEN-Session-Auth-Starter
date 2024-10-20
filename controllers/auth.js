9bf253273fbc8ed08ba7d960575f12e7800eb20a
Muhannad <muhanned9980@gmail.com>
2024-10-20 13:08:55 +0300

    adding the signup link and form

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,4 +3,8 @@ const express = require("express");
 //create routes using the  express content 
 const router = express.Router();
 // to berdge the two pages make link with the server.js 
+
+router.get('/sign-up' , async (req , res)=>{
+    res.render('auth/sign-up.ejs');
+})
 module.exports = router;
