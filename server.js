341c178e54e6daf19dbf76238b17bf301403245f
Muhannad <muhanned9980@gmail.com>
2024-10-20 11:21:52 +0300

    adding the controller and make the index for the sign-up

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
-const express = require('express');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-require('dotenv').config();
-require('./config/dataBase.js');
-
+const express = require("express");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+require("dotenv").config();
+require("./config/dataBase.js");
+// CONTROLLER
+const authController = require("./controllers/auth.js");
 const app = express();
 
 // Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT ? process.env.PORT : "3000";
 //MIDDLEWARE
-
-
 
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
-app.use(morgan('dev'));
-
-
-
+app.use(morgan("dev"));
 
 // ROUTES
 
 // server.js
 
-// GET /test the route 
+// GET /test the route
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
-  });
+  res.render("index.ejs");
+});
+
+app.use("/auth", authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
