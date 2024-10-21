f86bfc5521de789ded5bfb91b5510464a4dc1f73
Muhannad <muhanned9980@gmail.com>
2024-10-21 08:37:36 +0300

    fixing the bugs

--- config/auth.js
+++ config/auth.js
@@ -1,19 +1,13 @@
 // require the bcrypt
 const bcrypt = require("bcrypt");
 // facntion to encrypt the password
-function encryptPassword (password){
-    return bcrypt.hashSync(password, parseInt( process.env.SALT_ROUNDES));
-
+function encryptPassword (password) {
+    return bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS));
 }
-
-function comparedPassword(password, hashPassword){
-    return bcrypt.compareSync(password, hashPassword);
-
+function comparePassword (password, hashpassword) {
+    return bcrypt.compareSync(password, hashpassword);
 }
-
-
-
-module.exports={
+module.exports = {
     encryptPassword,
-    comparedPassword,
+    comparePassword,
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,75 +1,66 @@
 /* eslint-disable prefer-destructuring */
-const express = require("express");
-const User = require("../models/user");
-const auth = require("../config/auth");
+const express = require('express');
+const User = require('../models/user');
+const auth = require('../config/auth');
 
 const router = express.Router();
-// sign up
+
+//sign up
 router.get("/sign-up", async (req, res) => {
-  res.render("auth/sign-up.ejs");
+    res.render("auth/sign-up.ejs");
 });
 
 router.post("/sign-up", async (req, res) => {
-  // grab the values from the req body
-  const username = req.body.username;
-  const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
-  // Check if the user already exists
-  const existingUser = await User.findOne({ username });
+    const username = req.body.username;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
 
-  // if the user exists,then dont bother doing anything, just send a message to the browser
-  if (existingUser) {
-    return res.send("Username is taken");
-  }
-  // verify that the password matches
-  if (password !== confirmPassword) {
-    return res.send("Passwords don't match!");
-  }
+    const existingUser = await User.findOne({ username});
+    if (existingUser) {
+        return res.send("Username is taken");
+    }
 
-  // create the user in the database
-  // -b make the password secure
-  const hashPassword = auth.encryptPassword(password);
-  const payload = { username, password: hashPassword };
+    if (password  !== confirmPassword) {
+        return res.send ("password don't mattch");
+    }
 
-  const newUser = await User.create(payload);
-  // respond back to the browser
-  res.send(`Thanks for signing up ${newUser.username}`);
+    //make password secure
+    const hashPassword = auth.encryptPassword(password);
+    
+    const newUser = await User.create({username, password:hashPassword });
+
+    res.send(`Thanks for signing up ${newUser.username}`);
 });
 
-// sign IN
-router.get("/sign-in", (req, res) => {
-  res.render("auth/sign-in.ejs");
+//sign in
+router.get('/sign-in', (req, res) =>{
+    res.render('auth/sign-in.ejs')
 });
 
-router.post("/sign-in", async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const user = await User.findOne({ username });
-  //find the user from the user name
+router.post('/sign-in', async (req, res) => {
+const username = req.body.username;
+const password = req.body.password;
 
-  // if the user not exist send an error msg
-  if (!user) {
-    return res.send("login failed , please try again");
-  }
+const user = await User.findOne({username});
+if (!user) {
+    return res.send('Login failed. Please try again.');
+}
 
-  // compare the password submitted with the one in the databse
-  const validPassword = auth.comparedPassword(password, user.password);
-  if (!validPassword) {
-    return res.send("login failed , please try again");
-  }
+const validPassword = auth.comparePassword(password, user.password);
+if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+}
 
-  //if the password no good then send a error message
-  //create a session cookie
-  req.session.user = {
+//creat session cookis
+req.session.user = {
     username: user.username,
-  };
-
-  req.session.save(() => {
+}
+req.session.save(() => {
     res.redirect("/");
   });
 });
 
-//sign-out
+//sign out
 router.get("/sign-out", (req, res) => {
     req.session.destroy(() => {
         res.redirect("/");
